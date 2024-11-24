bd69a11aa16937cf390eddbbe027f8b6d5ab3332
Leandro Fusco <leandrofus@gmail.com>
2024-11-23 23:38:02 -0300

    ok

--- obj/Debug/net8.0/WebClient.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24fb791b25f6883f76d171ee7af2855097f585a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57410c875e6d7e0925cfe407df380ba380145f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
